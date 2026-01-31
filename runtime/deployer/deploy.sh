16181e20736c8e8cf7344b124d250032887d3a37
Rishat Alkhamov <43090692+alkhamov@users.noreply.github.com>
2026-01-31 23:28:17 +0100

    Update deploy.sh

--- runtime/deployer/deploy.sh
+++ runtime/deployer/deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 set -e
 
+cd /deploy   # <-- wichtig, weil WORKDIR ist /deploy im Container
+
 CAMUNDA_URL=http://camunda:8080/engine-rest
 
-echo "⏳ Waiting for Camunda to be ready..."
+echo "⏳ Waiting for Camunda..."
 until curl -s "$CAMUNDA_URL/engine" > /dev/null; do
   sleep 5
 done
 
-echo "✅ Camunda is ready, deploying BPMN/DMN..."
+echo "🚀 Deploying BPMN/DMN to Camunda..."
+
+curl -X POST "$CAMUNDA_URL/deployment/create" \
+  -F "deployment-name=custom-processes" \
+  -F "deployment-source=docker" \
+  -F "data=@bpmn/DMN_Test_1.bpmn" \
+  -F "data=@dmn/test_1.dmn"
+
 
 

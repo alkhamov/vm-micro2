c467ea775bf2dc911e9607bef201f74b7871ced3
Rishat Alkhamov <43090692+alkhamov@users.noreply.github.com>
2026-02-03 20:13:44 +0100

    Create init.sql

---
+++ postgres/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE requests (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  action TEXT NOT NULL,
+  use_case TEXT
+);
+
+INSERT INTO requests (name, action)
+VALUES
+  ('subscriber1', 'add'),
+  ('subscription', 'delete'),
+  ('subscriber2', 'add');
